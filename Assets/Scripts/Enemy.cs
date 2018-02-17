aad4d6432e517c20852df065a5610e3fbbc87343
Brendan Le <le.brendan@gmail.com>
2018-02-17 15:25:24 -0500

    Basic trap script

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -2,34 +2,32 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy : MonoBehaviour
+public class Enemy : Entity
 {
 
 	public float Speed = 5F;
-	public float MaxHealth = 100;
 
 	private Rigidbody2D _rigidbody;
-	private float _health;
 
 	// Use this for initialization
-	void Start ()
+	public void Start ()
 	{
+		base.Start();
 		_rigidbody = GetComponent<Rigidbody2D>();
-		_health = MaxHealth;
+		Health = MaxHealth;
 	}
 	
 	// Update is called once per frame
-	void Update ()
+	public void Update ()
 	{
+		base.Update();
 		_rigidbody.velocity = Vector2.right * Speed;
 	}
-
-	public void TakeDamage(float damage)
+	
+	private bool _isBlockedByBarrier()
 	{
-		_health -= damage;
-		if (_health <= 0)
-		{
-			Destroy(gameObject);
-		}
+        // Vector2 top_left = new Vector2(transform.position.x - 0.3F, transform.position.y - distToGround);
+        // Vector2 bot_right = new Vector2(transform.position.x + 0.3F, transform.position.y - distToGround - 0.3F);
+        // return Physics2D.OverlapArea(top_left, bot_right, groundLayers);
 	}
 }
---
+++ Assets/Scripts/Entity.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Entity : MonoBehaviour {
+	
+		
+	public float MaxHealth = 100;
+	public float Health { get; set; }
+	
+	// Use this for initialization
+	public void Start () {
+		
+		
+	}
+	
+	// Update is called once per frame
+	public void Update () {
+		
+	}
+
+	public void TakeDamage(float damage)
+	{
+		Health -= damage;
+		if (Health <= 0)
+		{
+			Destroy(gameObject);
+		}
+	}
+}
