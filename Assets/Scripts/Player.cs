3ad87fd36304449f64d3b56f57686341ade41190
MaximumLeaf <32802600+MaximumLeaf@users.noreply.github.com>
2018-02-24 15:24:06 -0500

    player sprite

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : Entity {
+
+	private Rigidbody2D _rigidbody;
+	private bool FireballCharge;
+
+	// Use this for initialization
+	void Start () {
+		base.Start();
+		_rigidbody = GetComponent<Rigidbody2D>();
+		Health = MaxHealth;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		for (int i = 0; i < Input.touchCount; ++i)
+		{
+			if (Input.GetTouch(i).phase == TouchPhase.Began)
+			{
+				bool FireballCharge = true;
+			}
+			//if (FireballCharge == true)
+			{
+
+			}
+			// Construct a ray from the current touch coordinates
+			//Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(i).position);
+			// Create a particle if hit
+			//if (Physics.Raycast(ray))
+				//Instantiate(Enemy, transform.position, transform.rotation);
+		}
+	}
+}
