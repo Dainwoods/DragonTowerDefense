e0813bad276105844faedd6a29c9b49f7a6565b1
Brendan Le <le.brendan@gmail.com>
2018-02-24 14:09:38 -0500

    Snap traps to ground

--- Assets/Scripts/Trap.cs
+++ Assets/Scripts/Trap.cs
@@ -10,10 +10,25 @@ public class Trap : Entity
 	// Use this for initialization
 	public void Start () {
 		base.Start();
+		
+		// Snap traps to ground.
+		float floorHeight = FloorHeight();
+		var collider = GetComponent<BoxCollider2D>();
+		float bottomHeight = collider.bounds.center.y - collider.bounds.size.y / 2;
+		float newY = transform.position.y - bottomHeight + floorHeight;
+		
+		transform.position = new Vector3(transform.position.x, newY, transform.position.z);
 	}
 	
 	// Update is called once per frame
 	public void Update () {
 		base.Update();
+	}
+
+	private float FloorHeight()
+	{
+		GameObject floor = GameObject.FindGameObjectsWithTag("Floor")[0];
+		BoxCollider2D floorCollider = floor.GetComponent<BoxCollider2D>();
+		return floorCollider.bounds.center.y + floorCollider.bounds.size.y / 2;
 	}
 }
