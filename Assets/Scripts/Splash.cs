0a528c53839c9ca6929dc9e8041289d0a84fc9b0
Brendan Le <le.brendan@gmail.com>
2018-03-10 15:50:39 -0500

    Add title screen image

--- Assets/Scripts/Splash.cs
+++ Assets/Scripts/Splash.cs
@@ -1,16 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Splash : MonoBehaviour {
-
 	// Use this for initialization
 	void Start () {
-		
+		Button btn = GetComponent<Button>();
+		btn.onClick.AddListener(OnClick);
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	void OnClick()
+	{
+		SceneManager.LoadScene("MainScene", LoadSceneMode.Single);
 	}
 }
